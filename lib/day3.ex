c0d3cc06c2eb4b6ae9988c70881759ac616efba4
Moritz Schmale <narrow.m@gmail.com>
2022-12-03 10:01:34 +0100

    apply set theory

--- lib/day3.ex
+++ lib/day3.ex
@@ -23,28 +23,21 @@ defmodule Day3 do
     input
     |> Stream.map(&String.trim/1)
     |> Stream.map(&split_evenly/1)
-    |> Stream.map(&rucksack_priority/1)
+    |> Stream.flat_map(&apply(MapSet, :intersection, &1))
+    |> Stream.map(&item_priority/1)
     |> Enum.sum()
     |> IO.puts()
   end
 
   def part_2(input \\ input_list()) do
     input
+    |> Stream.map(&String.trim/1)
     |> Stream.map(&String.to_charlist/1)
-    |> Stream.map(&Enum.uniq/1)
+    |> Stream.map(&MapSet.new/1)
     |> Stream.chunk_every(3)
-    |> Stream.map(&group_priority/1)
+    |> Stream.flat_map(fn sets -> Enum.reduce(sets, &MapSet.intersection/2) end)
+    |> Stream.map(&item_priority/1)
     |> Enum.sum()
-    |> IO.puts()
-  end
-
-  def group_priority(group) do
-    badge_item = Enum.reduce_while(?A..?z, nil, fn item, _acc ->
-      if Enum.all?(group, & item in &1),
-      do: {:halt, item},
-      else: {:cont, nil}
-    end)
-    item_priority(badge_item)
   end
 
   def split_evenly(string) do
@@ -52,25 +45,11 @@ defmodule Day3 do
     |> String.split_at(div(String.length(string), 2))
     |> Tuple.to_list()
     |> Enum.map(&String.to_charlist/1)
-    |> Enum.map(&Enum.uniq/1)
-    |> List.to_tuple()
+    |> Enum.map(&MapSet.new/1)
   end
 
   defcase item_priority() do
     l when l >= ?a and l <= ?z -> l - ?a + 1
     u when u >= ?A and u <= ?Z -> u - ?A + 27
-  end
-
-  def rucksack_priority({lhs, rhs}) do
-    for item <- lhs, reduce: 0 do
-      sum ->
-        if item in rhs do
-          prio = item_priority(item)
-          # IO.puts "#{[item]} is duplicate with priority #{prio}"
-          sum + prio
-        else
-          sum
-        end
-    end
   end
 end
