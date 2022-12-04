a4994062140fde6e8db8f357efd80329dd993111
Moritz Schmale <narrow.m@gmail.com>
2022-12-04 20:20:43 +0100

    day 4

---
+++ lib/day4.ex
@@ -0,0 +1,46 @@
+defmodule Day4 do
+  import Macros
+
+  def solve(input, fun) do
+    input
+    |> Stream.map(&String.trim/1)
+    |> Stream.reject(& &1 == "")
+    |> Stream.flat_map(&String.split(&1, ","))
+    |> Stream.flat_map(&String.split(&1, "-"))
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.chunk_every(2)
+    |> Stream.map(&apply(Range, :new, &1))
+    |> Stream.chunk_every(2)
+    |> Enum.count(&apply(fun, &1))
+  end
+
+  def part_1(input \\ File.stream!("./day4.txt")) do
+    solve(input, fn a, b ->
+      subset?(a, b) or subset?(b, a)
+    end)
+  end
+
+  def part_2(input \\ File.stream!("./day4.txt")) do
+    solve(input, &intersects?/2)
+  end
+
+  def subset?(f1..t1, f2..t2) do
+    f2 >= f1 and f2 <= t1 and t2 >= f1 and t2 <= t1
+  end
+
+  def intersects?(x1..x2, y1..y2) do
+    x2 >= y1 && x1 <= y2
+  end
+
+  def example do
+    """
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+    """
+    |> String.split("\n")
+  end
+end
