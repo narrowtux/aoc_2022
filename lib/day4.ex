32748d9f0610255ea6eb67d12183ec4d8f8805ca
Moritz Schmale <narrow.m@gmail.com>
2022-12-04 20:34:28 +0100

    found elixir stdlib function for disjoint

--- lib/day4.ex
+++ lib/day4.ex
@@ -2,7 +2,7 @@ defmodule Day4 do
   def solve(input, fun) do
     input
     |> Stream.map(&String.trim/1)
-    |> Stream.reject(& &1 == "")
+    |> Stream.reject(&(&1 == ""))
     |> Stream.flat_map(&String.split(&1, ~r/[,-]/))
     |> Stream.map(&String.to_integer/1)
     |> Stream.chunk_every(2)
@@ -12,19 +12,15 @@ defmodule Day4 do
   end
 
   def part_1(input \\ File.stream!("./day4.txt")) do
-    solve(input, &subset?(&1, &2) or subset?(&2, &1))
+    solve(input, &(subset?(&1, &2) or subset?(&2, &1)))
   end
 
   def part_2(input \\ File.stream!("./day4.txt")) do
-    solve(input, &intersects?/2)
+    solve(input, &(not Range.disjoint?(&1, &2)))
   end
 
   def subset?(f1..t1, f2..t2) do
     f2 >= f1 and f2 <= t1 and t2 >= f1 and t2 <= t1
-  end
-
-  def intersects?(x1..x2, y1..y2) do
-    x2 >= y1 && x1 <= y2
   end
 
   def example do
